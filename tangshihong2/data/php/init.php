8ec01f3756c23f7eb62acd66e7a325da9efce103
tsh <1134512154@qq.com>
2019-12-02 22:00:15 +0800

    修复: 更改php连接数据库的地址

--- tangshihong2/data/php/init.php
+++ tangshihong2/data/php/init.php
@@ -1,5 +1,10 @@
 <?php
  header("Content-Type:application/json;charset=utf-8");
- $conn = mysqli_connect("111.231.207.140","tsh","tsh","tsh",3306);
+ $host = "52.69.108.239";
+ $username = "tangshihong2";
+ $password = "tangshihong2";
+ $dbName = "tangshihong2";
+ $port = 3306;
+ $conn = mysqli_connect($host,$username,$password,$dbName,$port);
  mysqli_query($conn,"SET NAMES UTF8");
 ?>
