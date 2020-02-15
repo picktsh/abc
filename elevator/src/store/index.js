6918232376325d365911146bf2d49ef41547103a
tsh <1134512154@qq.com>
2020-02-15 15:28:29 +0800

    修改: 添加vuex.store

---
+++ elevator/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+  state: {
+    isLogin: false
+  },
+  actions: {},
+  mutations: {
+    login (state) {
+      state.isLogin = true
+    },
+    logout (state) {
+      state.isLogin = false
+    },
+  }
+})
+
+
+
