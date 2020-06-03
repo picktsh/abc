b93c2bf00ab6b446fdac9952b2b572fea8b135e4
rise <rise.666@qq.com>
2020-06-03 17:45:41 +0800

    webhook.js 处理push请求

--- webhook/deploy-dev.sh
+++ webhook/deploy-dev.sh
@@ -1,1 +1,5 @@
-ls
+# shellcheck disable=SC2164
+cd /www/wwwroot/abc
+git reset --hard origin/master
+git pull
+#重运行pm2相关任务 [聊天室,webhook服务]
--- webhook/webhook.js
+++ webhook/webhook.js
@@ -28,9 +28,21 @@ http.createServer((req, res) => {
 handler.on('error', (err) => {
   console.log('_*_Error_*_', err.message)
 })
-handler.on('*', event => {
-  console.log('Receive *')
-  run_cmd('sh', ['./deploy-dev.sh'], function (text) {
-    console.log(text)
-  })
+// handler.on('*', event => {
+//   console.log('Receive *')
+//   run_cmd('sh', ['./deploy-dev.sh'], function (text) {
+//     console.log(text)
+//   })
+// })
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+  // 分支判断
+  if (event.payload.ref === 'refs/heads/master') {
+    console.log('deploy master..')
+    run_cmd('sh', ['./deploy-dev.sh'], function (text) {
+      console.log(text)
+    })
+  }
 })
